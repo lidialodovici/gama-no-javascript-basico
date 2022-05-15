6f6afa5915417d5e1b542f8a443b7fdd294bf86a
Lídia Lodovici <lidiafernandes10@gmail.com>
2022-05-15 18:05:09 -0300

    aula desafio: validador II

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+function validaCPF(){
+  return false
+}
+
+
 function validacao(){
-  console.log('iniciando...')
+  const cpf = document.getElementById('cpf_digitado').value
+  console.log(cpf)
+  const resultadoValidacao = validaCPF(cpf)
+
+  if(resultadoValidacao){
+    document.getElementById("success").style.display = "block"
+  } else {
+    document.getElementById("error").style.display = "blockcle"
+  }
 }
