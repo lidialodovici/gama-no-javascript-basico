1307df970434e06a70d00cff7df53a694c7c78fe
Lídia Lodovici <lidiafernandes10@gmail.com>
2022-05-15 18:42:54 -0300

    aula: desafio validador cpf V

--- script.js
+++ script.js
@@ -11,12 +11,31 @@ function validaCPF(cpf){
     for(var i = 10; i > 1; i--){
       soma += numeros.charAt(10 - i) * i;
     }
+
+    console.log(soma)
     var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11)
     //validação do primeiro digito
     if(resultado != digitos.charAt(0)){
       return false;
     }
-    console.log(digitos.toString().charAt(0) + " é a primeira posição da variavel soma")
+
+    soma = 0;
+    numeros = cpf.substring(0, 10)
+
+    for(var k = 11; k > 1; k--){
+      soma += numeros.charAt(11 - k) * k;
+    }
+    resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+    //validação do segundo digito
+    if(resultado != digitos.charAt(1)){
+      return false;
+    }
+    console.log(soma)
+    // console.log(digitos.toString().charAt(0) + " é a primeira posição da variavel soma")
+
+
+
     return true;
 
   }
@@ -24,6 +43,8 @@ function validaCPF(cpf){
 
 
 function validacao(){
+  document.getElementById("success").style.display = "none"
+  document.getElementById("error").style.display = "none"
   const cpf = document.getElementById('cpf_digitado').value
   console.log(cpf)
   const resultadoValidacao = validaCPF(cpf)
