cee16291728fd966c743727e55cc0f3a2c1f0f72
Lídia Lodovici <lidiafernandes10@gmail.com>
2022-05-15 18:23:31 -0300

    aula: desafio validador III

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
-function validaCPF(){
-  return false
+function validaCPF(cpf){
+  //quantidade de caracteres
+  if(cpf.length != 11){
+    return false;
+  } else {
+    var numeros = cpf.substring(0, 9)
+    var digitos = cpf.substring(9)
+    
+    var soma = 0;
+    for(var i = 10; i < 1; i--){
+      soma += numeros.charAt(10 - i) * i;
+    }
+    console.log(soma)
+    return true;
+  }
 }
 
 
@@ -12,6 +25,6 @@ function validacao(){
   if(resultadoValidacao){
     document.getElementById("success").style.display = "block"
   } else {
-    document.getElementById("error").style.display = "blockcle"
+    document.getElementById("error").style.display = "block"
   }
 }
